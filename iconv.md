31d887c108e472cb4f85c4c71eb01ab2a761e83e
Ben <bensongsyz@gmail.com>
2019-12-22 20:26:05 +0800

    Tips from Windows

---
+++ iconv.md
@@ -0,0 +1,1 @@
+iconv -f GB2312 -t UTF-8 test1.m -o test1.m 
---
+++ iconv_shell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#echo "$#" != "2"
+
+if [ "$#" != "2" ];then
+	echo "Usage: `basename $0` dir filter"
+	echo "Usage: If filter contains '*' '?', use \"\" to bracket it."
+	exit
+fi
+
+dir=$1
+filter=$2
+echo $1
+for file in `find $dir -name "$2"`;do
+	echo "$file"
+	iconv -f GB2312 -t UTF-8 $file -o $file
+done
